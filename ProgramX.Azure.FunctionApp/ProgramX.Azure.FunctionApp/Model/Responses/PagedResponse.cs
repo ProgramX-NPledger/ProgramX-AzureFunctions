5fed87850f2a968efc95b2277bce00baf1fbec47
ProgramX-NPledger <nathan@programx.co.uk>
2025-08-18 22:04:50 +0100

    Changed response JSON property names

--- ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp/Model/Responses/PagedResponse.cs
+++ ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp/Model/Responses/PagedResponse.cs
@@ -1,10 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace ProgramX.Azure.FunctionApp.Model.Responses;
 
 public class PagedResponse<T>
 {
+    [JsonPropertyName("items")]
     public IEnumerable<T> Items { get; set; }
+    [JsonPropertyName("continuationToken")]
     public string? ContinuationToken { get; set; }
+    [JsonPropertyName("itemsPerPage")]
     public int? ItemsPerPage { get; set; }
+    [JsonPropertyName("isLastPage")]
     public bool IsLastPage { get; set; }
     
 }
