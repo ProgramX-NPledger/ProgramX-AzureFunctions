70a3c81097e21aa02490e5a4d723f431883b8cb1
ProgramX-NPledger <nathan@programx.co.uk>
2025-09-20 16:29:52 +0100

    Creating user confirmed (up to confirmation email send)

--- ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp/HttpTriggers/UsersHttpTrigger.cs
+++ ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp/HttpTriggers/UsersHttpTrigger.cs
@@ -456,7 +456,7 @@ public class UsersHttpTrigger : AuthorisedHttpTriggerBase
                 newUser.passwordHash = new byte[0];
                 newUser.passwordSalt = new byte[0];
                 
-                // send email with link to set password
+               // send email with link to set password
                 var client = new EmailClient(Configuration["AzureCommunicationServicesConnection"]);
 
                 var content = new EmailContent("Please complete your programx.co.uk login")
@@ -465,7 +465,7 @@ public class UsersHttpTrigger : AuthorisedHttpTriggerBase
 Hi,
 
 Please complete your programx.co.uk login by navigating to the following link:
-https://apps.programx.co.uk/complete-login?username={createUserRequest.userName}
+{Configuration["ClientUrl"]}/complete-login?username={createUserRequest.userName}&n={newUser.passwordConfirmationNonce}
 
 This link is valid until {newUser.passwordLinkExpiresAt}.
 
@@ -473,7 +473,7 @@ This link is valid until {newUser.passwordLinkExpiresAt}.
                     Html = @$"<h1>Please complete your programx.co.uk login</h1>
 <p>Hi,</p>
 <p>Please complete your programx.co.uk login by navigating to the following link:<br />
-<a href=""https://apps.programx.co.uk/complete-login?username={createUserRequest.userName}"">Complete login by entering your password</a></p>
+<a href=""{Configuration["ClientUrl"]}/complete-login?username={createUserRequest.userName}&n={newUser.passwordConfirmationNonce}"">Complete login by entering your password</a></p>
 <p>This link is valid until {newUser.passwordLinkExpiresAt}.</p>"
                 };
 
