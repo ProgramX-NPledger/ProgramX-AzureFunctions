1c1dc3a2c5b6900622ff3935e4a8e3f24d4f7b71
ProgramX-NPledger <nathan@programx.co.uk>
2025-12-02 14:05:40 +0000

    Improved model

--- ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp.Model/HealthCheckItemResult.cs
+++ ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp.Model/HealthCheckItemResult.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace ProgramX.Azure.FunctionApp.Model;
 
 public class HealthCheckItemResult
@@ -5,20 +7,24 @@ public class HealthCheckItemResult
     /// <summary>
     /// Whether the check is healthy. If <c>null</c>, the check has not been run yet.
     /// </summary>
+    [JsonPropertyName("isHealthy")]
     public bool? IsHealthy { get; set; }
     
     /// <summary>
     /// Friendly text describing the check.
     /// </summary>
+    [JsonPropertyName("friendlyName")]
     public required string FriendlyName { get; set; }
     
     /// <summary>
     /// Additional information about the check.
     /// </summary>
+    [JsonPropertyName("message")]
     public string? Message { get; set; }
 
     /// <summary>
     /// Name of the check
     /// </summary>
+    [JsonPropertyName("name")]
     public required string Name { get; set; }
 }
