27dd8a1a2010eb2cafc7bd7f2be7968e808ac11b
ProgramX-NPledger <nathan@programx.co.uk>
2026-01-17 14:12:49 +0000

    Added support for refresh tokens from OSM

---
+++ ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp.Osm/Model/OsmTokenRefreshResponse.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace ProgramX.Azure.FunctionApp.Osm.Model;
+
+public class OsmTokenRefreshResponse
+{
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+    
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+    
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+    
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; set; }
+}
