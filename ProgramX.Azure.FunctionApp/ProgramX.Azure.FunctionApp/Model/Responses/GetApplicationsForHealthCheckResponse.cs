9566e555814cda75d2ef38dc7ad52a45eef0e32b
ProgramX-NPledger <nathan@programx.co.uk>
2026-04-05 12:38:13 +0100

    Added response type for health checks for applications

--- ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp/Model/Responses/GetApplicationsForHealthCheckResponse.cs
+++ ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp/Model/Responses/GetApplicationsForHealthCheckResponse.cs
@@ -4,13 +4,25 @@ namespace ProgramX.Azure.FunctionApp.Model.Responses;
 
 public class GetApplicationsForHealthCheckResponse
 {
+    /// <summary>
+    /// Timestamp of the health check.
+    /// </summary>
     [JsonPropertyName("timeStamp")]
     public DateTime TimeStamp {
         get;
         set;
     }
     
+    /// <summary>
+    /// List of Applications that are eligible for health checks.
+    /// </summary>
     [JsonPropertyName("healthCheckServices")] 
     public IList<ApplicationHealthCheckService> HealthCheckServices { get; set; } = [];
 
+    /// <summary>
+    /// Whether the user making the request is elevated and all Applications are returned.
+    /// </summary>
+    [JsonPropertyName("isElevated")]
+    public bool IsElevated { get; set; } = false;
+    
 }
