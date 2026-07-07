d8aedb075bd8fc617b03a0cd25d970b8e838ba45
ProgramX-NPledger <nathan@programx.co.uk>
2026-07-07 17:19:05 +0100

    Removing fixing heaalth checks function - not needed, too complex, will just use logs

--- ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp.Contract/IApplicationHealthCheck.cs
+++ ProgramX.Azure.FunctionApp/ProgramX.Azure.FunctionApp.Contract/IApplicationHealthCheck.cs
@@ -13,11 +13,5 @@ public interface IApplicationHealthCheck
     /// <returns>A result indicating the health status.</returns>
     Task<HealthCheckResult> CheckHealthAsync();
     
-    /// <summary>
-    /// Attempts to fix the health issues.
-    /// </summary>
-    /// <param name="healthCheckResult"></param>
-    /// <returns></returns>
-    Task<FixApplicationHealthCheckResult> FixHealthAsync(HealthCheckResult healthCheckResult);
     
 }
